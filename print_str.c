c4339dce3657a0690dc4e9a59aba0aa392faef96
george-ashraf-88 <georgeashraf88@icloud.com>
2023-11-14 12:38:00 -0800

    m

---
+++ print_str.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the lenght of a string.
+ * @s: string
+ * Return: integer.
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != 0; x++)
+		;
+	return (x);
+
+}
+
+/**
+ * _strlenc - finds length of a string but for constant characters
+ * @s: string
+ * Return: integer
+ */
+
+int _strlenc(const char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != 0; x++)
+		;
+	return (x);
+}
