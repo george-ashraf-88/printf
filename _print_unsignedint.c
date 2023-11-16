020144f5cacfe18c442b1f07f203164bea11451d
george-ashraf-88 <georgeashraf88@icloud.com>
2023-11-16 04:04:26 -0800

    task3

---
+++ _print_unsignedint.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_unsigned - prints integer
+ * @args: argument to print
+ * Return: integer
+ */
+
+int print_unsignedint(va_list args)
+{
+	unsigned int n = va_arg(args, int);
+	int m, k = n % 10, digit, exp = 1;
+	int  i = 1;
+
+	n = n / 10;
+	m = n;
+
+	if (k < 0)
+	{
+		_putchar('-');
+		m = -m;
+		n = -n;
+		k = -k;
+		i++;
+	}
+	if (m > 0)
+	{
+		while (m / 10 != 0)
+		{
+			exp = exp * 10;
+			m = m / 10;
+		}
+		m = n;
+		while (exp > 0)
+		{
+			digit = m / exp;
+			_putchar(digit + '0');
+			m = m - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(k + '0');
+
+	return (i);
+}
--- main.h
+++ main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -9,5 +13,10 @@ int print_37(void);
 int print_i(va_list args);
 int print_d(va_list args);
 int print_bin(va_list val);
+int print_unsignedint(va_list args);
+int print_oct(va_list val);
+int print_hex(va_list val);
+int print_HEX(unsigned int num);
+
 
 #endif
